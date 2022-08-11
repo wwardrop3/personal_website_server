0c541fb4deb529e094f207ff9fb892cea91cb52e
Wil Wardrop <wwardrop@gmail.com>
2022-08-11 12:35:45 -0500

    fix

--- personalwebsite/settings.py
+++ personalwebsite/settings.py
@@ -61,7 +61,8 @@ REST_FRAMEWORK = {
 # THIS IS NEW
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://personal-website-client-ww.herokuapp.com'
 )
 
 # UPDATE THIS
